e5d0bf97c04cb8b05494d6c87ddfdd7836e0f0a9
Allan Oguis <allanoguis@gmail.com>
2024-09-22 21:12:07 +0800

    css test

--- allanoguis/src/app/pages/projects/conway/page.tsx
+++ allanoguis/src/app/pages/projects/conway/page.tsx
@@ -3,18 +3,19 @@ import React from "react";
 import GameOfLife from "@/projects/conway/ConwayEngine3";
 import styles from "@/css/conway.module.css";
 
+const gameProps = {
+  cellsize: 5,
+  width: 300,
+  height: 400,
+  isRunning: true,
+};
+
 const ConwayPage: React.FC = () => {
   return (
     <>
-      <h1 className="text-center p-3">Conway Game of Life v3.0</h1>
       <div className={styles.conwayPage}>
-        <GameOfLife
-          cellsize={5}
-          width={300}
-          height={400}
-          isRunning={true}
-          onGridUpdate={() => {}} // not using this for now
-        />
+        <h1 className="text-center p-3">Conway Game of Life v3.0</h1>
+        <GameOfLife {...gameProps} onGridUpdate={() => {}} />
       </div>
     </>
   );
