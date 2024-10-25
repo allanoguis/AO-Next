2022eba93e9c5ac8a7a348dabfe48eece681fcb5
Allan Oguis <allanoguis@gmail.com>
2024-10-26 07:21:07 +0800

    Update layout.tsx

--- allanoguis/src/app/layout.tsx
+++ allanoguis/src/app/layout.tsx
@@ -4,7 +4,7 @@ import SideNav from "../components/navbar/SideNav";
 import HeaderMobile from "../components/navbar/HeaderMobile";
 import WrapperMain from "../components/navbar/pageWrapper";
 import HeaderMain from "../components/navbar/HeaderMain";
-
+import { ThemeProvider } from "next-themes";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 
@@ -21,14 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="light">
-        <HeaderMain />
-        <HeaderMobile />
-        <div className="flex">
-          <SideNav />
-          <main className="flex-1">
-            <WrapperMain>{children}</WrapperMain>
-          </main>
-        </div>
+        <ThemeProvider>
+          <HeaderMain />
+          <HeaderMobile />
+          <div className="flex">
+            <SideNav />
+            <main className="flex-1">
+              <WrapperMain>{children}</WrapperMain>
+            </main>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
